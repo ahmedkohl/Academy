b75b8db3d5e3203dc685cb537aeca926364a78f3
ahmed kohla <ahmedaliasd3348745@gmail.com>
2021-02-28 00:06:55 +0200

    THIRD  commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,8 @@
 export const environment = {
   production: true,
 //apiUrl:"https://localhost:44315/api/"
+apiUrl:'http://5.9.162.27:89/api/'
 //apiUrl:"http://elrehab.com/api/"
 //apiUrl:"http://51.77.199.190:91/api/"
-apiUrl:'http://5.9.162.25:94/api/'
+//apiUrl:'http://5.9.162.25:94/api/'
 }
